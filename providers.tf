15d44386629f8ea5d595aeacb16824176c79a01b
Aaron Evans <aarone@outlook.com.au>
2024-08-19 16:22:27 +1000

    init

---
+++ providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4"
+    }
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "~> 2"
+    }
+    nsxt = {
+      source  = "vmware/nsxt"
+      version = "~> 3"
+    }
+    boundary = {
+      source  = "hashicorp/boundary"
+      version = "~> 1"
+    }
+  }
+}
