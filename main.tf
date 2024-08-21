57427e6bdc7038f11e5d689df8a3fd1188f12f53
Simon Lynch <simon.lynch@hashicorp.com>
2024-08-21 14:53:31 +1000

    move to iso

--- main.tf
+++ main.tf
@@ -2,66 +2,85 @@ locals {
   hostnames = [for v in range(var.cluster_size) : format("openshift-%s", v)]
 }
 
-# --- Get latest RHEL image value from HCP Packer
-data "hcp_packer_artifact" "this" {
-  bucket_name  = "base-rhel-9"
-  channel_name = "latest"
-  platform     = "vsphere"
-  region       = "Datacenter"
+# # --- Get latest RHEL image value from HCP Packer
+# data "hcp_packer_artifact" "this" {
+#   bucket_name  = "base-rhel-9"
+#   channel_name = "latest"
+#   platform     = "vsphere"
+#   region       = "Datacenter"
+# }
+
+# # --- Retrieve IPs for use by the load balancer and Openshift virtual machines
+# data "nsxt_policy_ip_pool" "this" {
+#   display_name = "10 - gcve-foundations"
+# }
+# resource "nsxt_policy_ip_address_allocation" "this" {
+#   for_each     = toset(var.hostnames)
+#   display_name = each.value
+#   pool_path    = data.nsxt_policy_ip_pool.this.path
+# }
+
+# resource "nsxt_policy_ip_address_allocation" "load_balancer" {
+#   display_name = "openshift-load-balancer"
+#   pool_path    = data.nsxt_policy_ip_pool.this.path
+# }
+
+
+
+data "vsphere_datacenter" "datacenter" {
+  name = "Datacenter"
 }
 
-# --- Retrieve IPs for use by the load balancer and Openshift virtual machines
-data "nsxt_policy_ip_pool" "this" {
-  display_name = "10 - gcve-foundations"
-}
-resource "nsxt_policy_ip_address_allocation" "this" {
-  for_each     = toset(var.hostnames)
-  display_name = each.value
-  pool_path    = data.nsxt_policy_ip_pool.this.path
+data "vsphere_datastore" "datastore" {
+  name          = "vsanDatastore"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-resource "nsxt_policy_ip_address_allocation" "load_balancer" {
-  display_name = "openshift-load-balancer"
-  pool_path    = data.nsxt_policy_ip_pool.this.path
+data "vsphere_compute_cluster" "cluster" {
+  name          = "cluster"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
+data "vsphere_network" "network" {
+  name          = "seg-general"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
 
-# --- Deploy a cluster of Openshift servers
-module "openshift_server" {
+# deploy shell vm and attach iso using the vsphere vm resource not the module
+resource "vsphere_virtual_machine" "vm" {
   for_each = toset(var.hostnames)
-  source   = "app.terraform.io/tfo-apj-demos/virtual-machine/vsphere"
-  version  = "~> 1.3"
+  name             = each.value
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 12
+  memory           = 32768
+  guest_id         = "otherLinux64Guest"
+  folder = "Datacenter/vm/Demo Management"
 
-  num_cpus          = 12
-  memory            = 32768
-  hostname          = each.value
-  datacenter        = "Datacenter"
-  cluster           = "cluster"
-  primary_datastore = "vsanDatastore"
-  folder_path       = "Demo Workloads"
-  networks = {
-    "seg-general" : "${nsxt_policy_ip_address_allocation.this[each.value].allocation_ip}/22"
+  network_interface {
+    network_id = data.vsphere_network.network.id
   }
-  dns_server_list = [
-    "172.21.15.150",
-    "10.10.0.8"
-  ]
-  gateway         = "172.21.12.1"
-  dns_suffix_list = ["hashicorp.local"]
+  disk {
+    label = "disk0"
+    size  = 150
+    thin_provisioned = true
+  }
 
-  template = data.hcp_packer_artifact.this.external_identifier
-  tags = {
-    "application" = "openshift-server"
+   cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path         = "ISO/b649ce88-11c8-4504-9c72-3f1729fbaa02-discovery.iso"
   }
 }
 
-module "ssh_role" {
-  source  = "app.terraform.io/tfo-apj-demos/ssh-role/vault"
-  version = "0.0.4"
 
-  # insert required variables here
-  ssh_role_name = "${var.TFC_WORKSPACE_ID}-openshift_ssh_ca_signing"
-}
+
+# module "ssh_role" {
+#   source  = "app.terraform.io/tfo-apj-demos/ssh-role/vault"
+#   version = "0.0.4"
+
+#   # insert required variables here
+#   ssh_role_name = "${var.TFC_WORKSPACE_ID}-openshift_ssh_ca_signing"
+# }
 
 # --- Add LB to DNS <<<< TO REMOVE THIS
 # module "load_balancer_dns" {
@@ -76,13 +95,13 @@ module "ssh_role" {
 #   ]
 # }
 
-# --- Add servers to DNS
-module "openshift_server_dns" {
-  source  = "app.terraform.io/tfo-apj-demos/domain-name-system-management/dns"
-  version = "~> 1.0"
+# # --- Add servers to DNS
+# module "openshift_server_dns" {
+#   source  = "app.terraform.io/tfo-apj-demos/domain-name-system-management/dns"
+#   version = "~> 1.0"
 
-  a_records = [for host in module.openshift_server : {
-    "name"      = host.virtual_machine_name
-    "addresses" = [host.ip_address]
-  }]
-}
+#   a_records = [for host in module.openshift_server : {
+#     "name"      = host.virtual_machine_name
+#     "addresses" = [host.ip_address]
+#   }]
+# }
