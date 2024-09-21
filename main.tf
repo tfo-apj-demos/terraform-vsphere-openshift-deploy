27ab7d32f01587cafc31a4269f2249135d02f4a9
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-21 17:10:14 +1000

    Update VM folder from "Demo Workloads" to "Demo Management"

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "vsphere_virtual_machine" "vm" {
   num_cpus         = 12
   memory           = 32768
   guest_id         = "otherLinux64Guest"
-  folder           = "Demo Management"
+  folder           = "Demo Workloads"
   scsi_type        = "pvscsi"
   enable_disk_uuid = true
 
